e330d451f3261e46ee33f9f5598882a8920d2b2d
Kyle Mathews <mathews.kyle@gmail.com>
2024-01-18 16:59:01 -0800

    Cleanup dummy sst stuff

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,29 +1,17 @@
-import { StackContext, Api, EventBus, Config } from "sst/constructs"
+import { StackContext, Api, Config } from "sst/constructs"
 
 export function API({ stack }: StackContext) {
-  const bus = new EventBus(stack, `bus`, {
-    defaults: {
-      retries: 10,
-    },
-  })
-
   const CONNECTION_STRING_SECRET = new Config.Secret(stack, `CONNECTION_STRING`)
 
   const api = new Api(stack, `api`, {
     defaults: {
       function: {
-        bind: [bus, CONNECTION_STRING_SECRET],
+        bind: [CONNECTION_STRING_SECRET],
       },
     },
     routes: {
       "GET /": `packages/functions/src/lambda.handler`,
-      "GET /todo": `packages/functions/src/todo.list`,
-      "POST /todo": `packages/functions/src/todo.create`,
     },
-  })
-
-  bus.subscribe(`todo.created`, {
-    handler: `packages/functions/src/events/todo-created.handler`,
   })
 
   stack.addOutputs({
